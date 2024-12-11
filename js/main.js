0758114ec4a8123db4b0782450086dc199c68b09
AlmuDiazMadramany <almumadra@hotmail.com>
2024-12-11 11:37:04 +0100

    conditionals

--- js/main.js
+++ js/main.js
@@ -4,8 +4,10 @@ const catlist = document.querySelector(".js-list");
 const buttonAdd = document.querySelector(".js-btn-add");
 const newForm = document.querySelector(".js-form");
 const buttonCancel = document.querySelector(".js-cancel");
-const formCats = document.querySelector(".js-formcats")
-//const inputCleane = document.querySelector(".input");
+const formCats = document.querySelector(".js-formcats");
+const searchButton = document.querySelector(".js-button-search");
+const descrSearchText = docuemnt.querySelector(".js-in-search-desc");
+const resultsContainer = document.getElementById("js-data-container");
 
 const kittenOne = `<li class="card">
 <article>
@@ -63,3 +65,32 @@ buttonCancel.addEventListener("click", () => {
   //Entonces había que poner una clase al elemento form
 });
 
+
+searchButton.addEventListener("click", (event) => {
+  event.preventDefault();
+  const descrSearchText = input_search_desc.value;
+});
+
+const kittenDesc1 =
+  "Porte elegante, su patrón de color tan característico y sus ojos de un azul intenso, pero su historia se remonta a Asía al menos hace 500 años, donde tuvo su origen muy posiblemente.";
+
+
+const kittenDesc2 =
+  "Produce fascinación y curiosidad. Exótico, raro, bello, extraño… hasta con pinta de alienígena han llegado a definir a esta raza gatuna que se caracteriza por la «ausencia» de pelo.";
+
+const kittenDesc3 =
+  "Tienen la cabeza cuadrada y los ojos simétricos, por lo que su bella mirada se ha convertido en una de sus señas de identidad. Sus ojos son grandes y las orejas resultan largas y en punta.";
+
+  if (kittenDesc1.includes(descrSearchText)) {
+    console.log("KittenDesc1")
+  }
+  
+  if (kittenDesc2.includes(descrSearchText)) {
+    console.log("KittenDesc1")
+  }
+  
+  if (kittenDesc3.includes(descrSearchText)) {
+    console.log("KittenDesc1")
+  }
+
+  // PERO NO SALEN LOS GATOS
